b0ba666724259bf7fb1810dcecda26c9b29e75ac
ArekPsurski <artopsu@gmail.com>
2022-04-23 07:12:24 +0200

    Wersja z inputem na spacje

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from math import factorial
 def binomial(a,b):
     return factorial(a) // factorial(b) // factorial(a-b)
-a = int(input())
-b = int(input())
+a, b = map(int, input().split())
 print(binomial(a,b))
